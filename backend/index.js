182325d807d0b191755c4339bc890aaf6b20820c
itz_Malc0m <danieldimfwina@gmail.com>
2024-04-09 20:40:58 +0100

    Connected to mongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+// import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    () => {console.log("Mongo is active...")}
+).catch((err) => {
+    console.log(err)
+})
 
 const app =  express();
 
